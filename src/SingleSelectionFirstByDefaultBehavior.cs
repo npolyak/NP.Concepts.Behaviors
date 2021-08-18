302431d97c27be3e46705c57e79a2e1fa5f4e292
npolyak <nick.polyak@verizon.net>
2021-08-18 18:28:18 -0400

    added generic behaviors

---
+++ src/SingleSelectionFirstByDefaultBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NP.Concepts.Behaviors
+{
+    public class SingleSelectionFirstByDefaultBehavior<TSelectable> : 
+        SingleSelectionBehavior<TSelectable>
+        where TSelectable : class, ISelectableItem<TSelectable>
+    {
+        public SingleSelectionFirstByDefaultBehavior()
+        {
+
+        }
+
+        public SingleSelectionFirstByDefaultBehavior(IEnumerable<TSelectable> collection) : base(collection)
+        {
+
+        }
+
+        public void SelectFirst()
+        {
+            this.TheCollection?.FirstOrDefault()?.Select();
+        }
+
+        protected override void OnCollectionSet()
+        {
+            base.OnCollectionSet();
+
+            SelectFirst();
+        }
+
+        protected override void BeforeItemAdded(TSelectable item)
+        {
+            base.BeforeItemAdded(item);
+
+            if (TheSelectedItem == null)
+            {
+                item.Select();
+            }
+        }
+
+        protected override void DoOnSelectedItemRemoved()
+        {
+            base.DoOnSelectedItemRemoved();
+
+            SelectFirst();
+        }
+    }
+}
