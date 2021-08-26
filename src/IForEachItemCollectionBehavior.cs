1458f75e35f5d3e3a72367029a419a8d06ecc209
npolyak <nick.polyak@verizon.net>
2021-08-26 17:08:41 -0400

    added a copyright notice to all source files

--- src/IForEachItemCollectionBehavior.cs
+++ src/IForEachItemCollectionBehavior.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿// (c) Nick Polyak 2021 - http://awebpros.com/
+// License: MIT License (https://opensource.org/licenses/MIT)
+//
+// short overview of copyright rules:
+// 1. you can use this framework in any commercial or non-commercial 
+//    product as long as you retain this copyright message
+// 2. Do not blame the author of this software if something goes wrong. 
+// 
+// Also, please, mention this software in any documentation for the 
+// products that use it.
+
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
 
 namespace NP.Concepts.Behaviors
 {
