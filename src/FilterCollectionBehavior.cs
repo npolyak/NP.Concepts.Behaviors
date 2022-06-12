279c6dc600db4b6ca94eb49a648dfcbbc401d354
npolyak <nick.polyak@verizon.net>
2022-06-12 19:44:13 -0400

    added important behaviors

---
+++ src/FilterCollectionBehavior.cs
@@ -0,0 +1,75 @@
+﻿using NP.Utilities;
+using NP.Utilities.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace NP.Concepts.Behaviors
+{
+    public class FilterCollectionBehavior<T>
+    {
+        private Func<T, bool> _filter;
+        public Func<T, bool> Filter 
+        {
+            get => _filter;
+            
+            set
+            {
+                _filter = value;
+                SetBehavior();
+            }
+        }
+
+
+        IDisposable _filteringBehavior;
+
+        private IEnumerable<T> _inputCollection;
+        [DataPoint(DataPointDirection.Source)]
+        public IEnumerable<T> InputCollection
+        {
+            get => _inputCollection;
+
+            set
+            {
+                if (ReferenceEquals(InputCollection, value))
+                    return;
+
+                _inputCollection = value;
+
+                SetBehavior();
+            }
+        }
+
+        private void SetBehavior()
+        {
+            _filteringBehavior?.Dispose();
+
+            OutputCollection.RemoveAllTyped();
+
+            _filteringBehavior = _inputCollection.AddBehavior(OnItemAdded, OnItemRemoved);
+        }
+
+        [DataPoint(DataPointDirection.Target)]
+        public ObservableCollection<T> OutputCollection { get; } = new ObservableCollection<T>();
+
+        private void OnItemAdded(T item)
+        {
+            if (_filter?.Invoke(item) == true)
+            {
+                OutputCollection.Add(item);
+            }
+        }
+
+        private void OnItemRemoved(T item)
+        {
+            if (_filter?.Invoke(item) == true)
+            {
+                OutputCollection.Remove(item);
+            }
+        }
+
+        public FilterCollectionBehavior()
+        {
+        }
+    }
+}
---
+++ src/TransformCollectionItemsBehavior.cs
@@ -0,0 +1,66 @@
+﻿using NP.Utilities;
+using NP.Utilities.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace NP.Concepts.Behaviors
+{
+    public class TransformCollectionItemsBehavior<TInput, TOutput>
+    {
+        public Func<TInput, TOutput> Transformer { get; }
+
+        public Func<TInput, TOutput, bool> Matcher { get; }
+
+        private IEnumerable<TInput> _inputCollection;
+
+        private IDisposable _transformBehavior;
+
+        [DataPoint(DataPointDirection.Source)]
+        public IEnumerable<TInput> InputCollection
+        {
+            get => _inputCollection;
+
+            set
+            {
+                if (ReferenceEquals(InputCollection, value))
+                    return;
+
+                _inputCollection = value;
+
+                _transformBehavior?.Dispose();
+
+                _transformBehavior = _inputCollection?.AddBehavior(OnItemAdded, OnItemRemoved);
+            }
+        }
+
+        private void OnItemAdded(TInput inputItem)
+        {
+            TOutput outputItem = Transformer(inputItem);
+
+            OutputCollection.Add(outputItem);
+        }
+
+        private void OnItemRemoved(TInput inputItem)
+        {
+            TOutput outputItemToRemove =
+                OutputCollection.FirstOrDefault(outputItem => Matcher(inputItem, outputItem));
+
+            OutputCollection.Remove(outputItemToRemove);
+        }
+
+        [DataPoint(DataPointDirection.Target)]
+        public ObservableCollection<TOutput> OutputCollection { get; } = 
+            new ObservableCollection<TOutput>();
+        
+        public TransformCollectionItemsBehavior
+        (
+            Func<TInput, TOutput> transformer,
+            Func<TInput, TOutput, bool> matcher)
+        {
+            Transformer = transformer;  
+            Matcher = matcher;
+        }
+    }
+}
